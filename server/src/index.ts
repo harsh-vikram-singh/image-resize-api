dc8cc72c13c51f816d67db8649fa57476ec5475a
Harsh Vikram Singh <singh.harshvikram@gmail.com>
2021-06-13 01:24:50 +0530

    initial setup with typescript and basic server with express

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('succfully accessed GET /');
+});
+
+app.listen(port, () => {
+  console.log('Welcome to image resize service');
+})
